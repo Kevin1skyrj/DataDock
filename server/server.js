649e87fa61c96ec4be9b4e20a1833a4c3d4a1c80
Rajat Pandey <rajatpndey257@gmail.com>
2026-08-13 23:11:10 +0530

    uninstall some unnecessary dependecies

--- server/server.js
+++ server/server.js
@@ -4,14 +4,14 @@ import { connectToDatabase } from "./src/config/db.js";
 const port = process.env.PORT || 4000;
 
 async function startServer() {
-    try{
-        await connectToDatabase();
-        app.listen(port, () =>{
-            console.log(`Server started on port ${port}`);
-        });
-    }catch(error){
-        console.log('Failed to start the server:', error.message);
-        process.exit(1);
-    }
+  try {
+    await connectToDatabase();
+    app.listen(port, () => {
+      console.log(`Server started on port ${port}`);
+    });
+  } catch (error) {
+    console.log("Failed to start the server:", error.message);
+    process.exit(1);
+  }
 }
 startServer();
